2d83de1c9558ec1bc44229fc1b1be05928aa90e6
hadiglitch10 <hadiunknownglitch@gmail.com>
2024-07-12 18:50:34 +0300

    besm allah

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,7 +2,7 @@
 -- script can be executed on any database
 CREATE TABLE IF NOT EXISTS `users` (
     `id` INT NOT NULL PRIMARY KEY AUTOINCREAMENT,
-    `email` VARCHAR (255) NOT NULL UNIQUE,
-    `name` VARCHAR (255),
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
     `country` ENUM('US','CO','TN') NOT NULL
 )

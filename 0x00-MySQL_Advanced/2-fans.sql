b67ab2b2c24f87470de08b7bafc9392216a5e0ab
hadiglitch10 <hadiunknownglitch@gmail.com>
2024-07-12 19:02:50 +0300

    besm allah

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
---rank countries bands by thier num fans
-SELECT origin, SUM(fans) AS nb_fans 
+-- Ranks country origins of bands, ordered by the number of (non-unique) fans.
+SELECT origin, SUM(fans) AS nb_fans
     FROM metal_bands
     GROUP BY origin
     ORDER BY nb_fans DESC;
